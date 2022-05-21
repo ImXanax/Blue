a523fab71280d9cc1eabd7bc8bbf55e399cfad9b
Xanax <Merwwj@gmail.com>
2022-05-21 13:04:06 +0430

    added user option for roles cmd

--- src/commands/Utility/roles.js
+++ src/commands/Utility/roles.js
@@ -8,28 +8,43 @@ const {
 } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("purge")
-    .setDescription("clears messages"),
+    .setName("roles")
+    .setDescription("displays all the roles you have equipped")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription(`the user you'd like to see their roles`)
+        .setRequired(false)
+    ),
   async execute(ctx, client) {
     const isAdmin = ctx.member.roles.cache.has("734431567912370196");
     if (isAdmin) {
-      let roles = ctx.member._roles;
       let allRoles = [];
-      let userRoles = '';
+      let userRoles = "";
+      let roles;
+
+      let mentionedUser = ctx.options.getUser("user");
+      if (mentionedUser) {
+        await ctx.guild.members
+          .fetch(mentionedUser.id)
+          .then((u) => roles = u._roles)
+          .catch(console.error);
+        }else{
+          roles = ctx.member._roles;
+        }
+
       for (role of roles) {
         let mentionableRole = `<@&${role}>`;
         allRoles.push(mentionableRole);
       }
-      
+
       for (let i = 0; i < allRoles.length; i++) {
         userRoles += ` ${allRoles[i]} `;
-        console.log(`${i}) ${allRoles[i]}`);
       }
-      console.log(userRoles)
-      const purgeEmbed = new MessageEmbed()
+      const rolesEmbed = new MessageEmbed()
         .setDescription(`**USER ROLES:** ${userRoles}`)
         .setColor("#0014e9");
-      ctx.reply({ embeds: [purgeEmbed] });
+      ctx.reply({ embeds: [rolesEmbed] });
     } else {
       ctx.reply({ content: `You Don't Have Permissions` });
     }
