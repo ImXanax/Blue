92b9db2990b61490ffeff1988d189385654dd561
Xanax <Merwwj@gmail.com>
2022-05-28 22:26:35 +0430

    removed user option from rc.js

--- src/commands/Moderation/rolecolor.js
+++ src/commands/Moderation/rolecolor.js
@@ -21,34 +21,14 @@ module.exports = {
         .setName("role")
         .setDescription("the role you want its color changed")
         .setRequired(false)
-    )
-    .addUserOption((option) =>
-      option
-        .setName("user")
-        .setDescription(`the user you'd like to see their roles`)
-        .setRequired(false)
     ),
   async execute(ctx, client) {
     const isAdmin = ctx.member.roles.cache.has("734431567912370196");
     if (isAdmin) {
-      //vars , options for: {USER} {HEX} {ROLE}
+      //vars , options for:{HEX} {ROLE}
       const hex = ctx.options.getString("hex");
       const r = ctx.options.getRole("role");
-      const user = ctx.options.getUser("user");
       const oldRoleColor = r.hexColor;
-      let u;
-      let hasRole = false;
-
-      //user
-      if (user) {
-        try {
-          u = await ctx.guild.members.fetch(user.id);
-        } catch (e) {
-          console.error;
-        }
-      } else {
-        u = ctx.member;
-      }
 
       //hex & formatting
       let h = hex.startsWith("#", 0);
@@ -59,18 +39,19 @@ module.exports = {
         return ctx.reply(
           `**__${hexColor}__** is not a correct hexidecimal color code`
         );
-      //u has the role?
-      if (u.roles.cache.has(r.id)) {
-        r.setColor(hexColor).then(()=>{
+
+      //change color of role
+      r.setColor(hexColor)
+        .then(() => {
           const embed = new MessageEmbed()
             .setDescription(
               `${r}'s color changed\n\`${oldRoleColor}\`==>\`${r.hexColor}\``
             )
             .setColor("#0014e9");
 
-          ctx.reply({embeds:[embed]})
-        }).catch((e)=> console.error(e))
-      }
+          ctx.reply({ embeds: [embed] });
+        })
+        .catch((e) => console.error(e));
     } else {
       ctx.reply({ content: `You Don't Have Permissions` });
     }
