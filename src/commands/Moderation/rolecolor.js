717152902af268f7e81fadfddcb47f7569efb246
Xanax <Merwwj@gmail.com>
2022-05-28 15:46:28 +0430

    hexRegex + Formatting

--- src/commands/Moderation/rolecolor.js
+++ src/commands/Moderation/rolecolor.js
@@ -10,24 +10,48 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("rolecolor")
     .setDescription("changes the role color of the specified role")
+    .addStringOption((option) =>
+      option
+        .setName("hex")
+        .setDescription("the hex color code")
+        .setRequired(true)
+    )
     .addUserOption((option) =>
       option
         .setName("user")
         .setDescription(`the user you'd like to see their roles`)
         .setRequired(false)
-    ).addStringOption(),
+    )
+    .addRoleOption((option) =>
+      option
+        .setName("role")
+        .setDescription("the role you want its color changed")
+        .setRequired(false)
+    ),
   async execute(ctx, client) {
     const isAdmin = ctx.member.roles.cache.has("734431567912370196");
     if (isAdmin) {
-        //options for , {USER} {HEX} {ROLE}
+      //options for , {USER} {HEX} {ROLE}
+      const user = ctx.options.getUser("user");
+      const hex = ctx.options.getString("hex");
+      const r = ctx.options.getRole("role");
+      //input management
+      // user
+      const u = user ? user : ctx.user;
+      //hex & formatting
+      let h = hex.startsWith("#", 0);
+      h = h ? h : `#${hex}`;
+      const regHex = new RegExp(/#[0-9a-f]{6}/, "i");
+      const isHex = regHex.test(h);
+      if(!isHex) return ctx.reply(`**__${h}__** is not a correct hexidecimal color code`)
+      
+      //IF USER
 
-        //IF USER
+      //NO USER - user = ctx emitter
 
-        //NO USER - user = ctx emitter
+      //GET ROLE
 
-        //GET ROLE
-
-        //RESULT
+      //RESULT
     } else {
       ctx.reply({ content: `You Don't Have Permissions` });
     }
