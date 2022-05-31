75451b846d87b2bdb37ec5ac9845b004ad8d8719
Xanax <Merwwj@gmail.com>
2022-05-31 20:36:22 +0430

    added logger for DM messages

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,6 +3,18 @@ const {MessageEmbed} = require("discord.js");
 module.exports = {
     name: 'messageCreate',
     on: true,
-    async execute(client,message){
+    async execute(message,client){
+        if(message.channel.type === 'DM'){
+           const dmEmbed = new MessageEmbed()
+            .setTitle(`DM FROM: ${message.author.tag}`)
+            .setDescription(`**__MESSAGE:__**\n\`\`\`${message.content}\`\`\``)
+            .setThumbnail(message.author.displayAvatarURL({format:'jpg'}))
+            .setFooter({text:`ID: ${message.author.id} | ${message.createdAt}`})
+            .setColor('#ffff00')
+            client.channels.fetch('762670306824290321').then((chl)=>{
+                chl.send({embeds:[dmEmbed]})
+            }).catch(e=>console.log(`ERR FETCHING CHANNEL: ${e}`))
+        }
+       
     }
 };
