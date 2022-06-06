46e3075479c09abb58fe7f86c936f15eb484e445
Xanax <Merwwj@gmail.com>
2022-06-06 12:33:21 +0430

    Levels/createUser function

--- src/funcs/Levels.js
+++ src/funcs/Levels.js
@@ -16,10 +16,23 @@ class Levels {
     });
   }
 
+  /**
+   * @param {string} [userId] - the user's Id.
+   * @param {string} [guildId] - the user's server Id.
+   */
   static async createUser(userId,guildId){
     if (!userId) throw new TypeError(`userId wasn't provided`);
     if (!guildId) throw new TypeError(`guildId wasn't provided`);
-    const isUser = await levelSchema.findOne({})
+    const isUser = await levelSchema.findOne({userID:userId , guildID: guildId})
+    if(isUser) return false
+
+    const newUser = new levelSchema({
+        userID: userId,
+        guildID: guildId,
+    })
+
+    await newUser.save().catch(e => console.error(`ERR IN CREATING USER: ${e}`))
+    return newUser
   }
 }
 
