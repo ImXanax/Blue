3b62aac6c9cf328ae54a73c5fb0d73cee6897893
Xanax <Merwwj@gmail.com>
2022-06-19 11:02:39 +0430

    calc.js/ Buttons + Embed

--- src/commands/Utility/calculator.js
+++ src/commands/Utility/calculator.js
@@ -16,30 +16,103 @@ module.exports = {
     /*
      *
      */
-    const r1 = new MessageActionRow().addComponents(
-      new MessageButton().setCustomId("7").setLabel("7").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("8").setLabel("8").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("9").setLabel("9").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("p").setLabel("+").setStyle("SECONDARY")
-    );
-    const r2 = new MessageActionRow().addComponents(
-      new MessageButton().setCustomId("4").setLabel("4").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("5").setLabel("5").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("6").setLabel("6").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("m").setLabel("-").setStyle("SECONDARY")
-    );
-    const r3 = new MessageActionRow().addComponents(
-      new MessageButton().setCustomId("1").setLabel("1").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("2").setLabel("2").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("3").setLabel("3").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("t").setLabel("x").setStyle("SECONDARY")
-    );
-    const r4 = new MessageActionRow().addComponents(
-      new MessageButton().setCustomId("c").setLabel("C").setStyle("DANGER"),
-      new MessageButton().setCustomId("0").setLabel("0").setStyle("PRIMARY"),
-      new MessageButton().setCustomId("e").setLabel("=").setStyle("SECONDARY"),
-      new MessageButton().setCustomId("d").setLabel("/").setStyle("SECONDARY")
-    );
-    ctx.reply({ components: [r1, r2, r3,r4] , fetchReply: true },);
+    const rows = [
+      new MessageActionRow().addComponents([
+        new MessageButton({
+          customId: "7",
+          style: "PRIMARY",
+          label: "7",
+        }),
+        new MessageButton({
+          customId: "8",
+          style: "PRIMARY",
+          label: "8",
+        }),
+        new MessageButton({
+          customId: "9",
+          style: "PRIMARY",
+          label: "9",
+        }),
+        new MessageButton({
+          customId: "p",
+          style: "SECONDARY",
+          label: "+",
+        }),
+      ]),
+      new MessageActionRow().addComponents([
+        new MessageButton({
+          customId: "4",
+          style: "PRIMARY",
+          label: "4",
+        }),
+        new MessageButton({
+          customId: "5",
+          style: "PRIMARY",
+          label: "5",
+        }),
+        new MessageButton({
+          customId: "6",
+          style: "PRIMARY",
+          label: "6",
+        }),
+        new MessageButton({
+          customId: "m",
+          style: "SECONDARY",
+          label: "-",
+        }),
+      ]),
+      new MessageActionRow().addComponents([
+        new MessageButton({
+          customId: "1",
+          style: "PRIMARY",
+          label: "1",
+        }),
+        new MessageButton({
+          customId: "2",
+          style: "PRIMARY",
+          label: "2",
+        }),
+        new MessageButton({
+          customId: "3",
+          style: "PRIMARY",
+          label: "3",
+        }),
+        new MessageButton({
+          customId: "t",
+          style: "SECONDARY",
+          label: "x",
+        }),
+      ]),
+      new MessageActionRow().addComponents([
+        new MessageButton({
+          customId: "c",
+          style: "DANGER",
+          label: "C",
+        }),
+        new MessageButton({
+          customId: "0",
+          style: "PRIMARY",
+          label: "0",
+        }),
+        new MessageButton({
+          customId: "e",
+          style: "SECONDARY",
+          label: "=",
+        }),
+        new MessageButton({
+          customId: "d",
+          style: "SECONDARY",
+          label: "/",
+        }),
+      ]),
+    ];
+
+    const msg = await ctx.reply({
+      components: rows,
+      embeds:[{
+        description:"\`\`\`RESULTS ARE DISPLAYED HERE.\`\`\`"
+      }],
+      fetchReply: true
+    })
   },
 };
